929d3c0db767ed073c64cbce3a6cf268a22088f8
Work & School <chouac@live.com>
2020-08-17 11:40:56 -0500

    init

---
+++ DemoWebApp/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebApp.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; } //[Key] ID's that this is the primary key.
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
